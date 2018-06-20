0be8f7fb149bdbf2ddbcff136bc10335487022eb
fsuciue <fsuciue@gmail.com>
2018-06-20 04:09:04 -0400

    React

---
+++ curs28-React Intro/script.js
@@ -0,0 +1,12 @@
+// const element = React.createElement(
+//   "h1", 
+//   {id:"grupa4"},
+//   "Hello World!"
+// );
+
+// ReactDOM.render(element, document.getElementById("root"));
+
+ReactDOM.render (
+  <h1 id="grupa4">Hello World!</h1>,
+  document.getElementById("root")
+);
